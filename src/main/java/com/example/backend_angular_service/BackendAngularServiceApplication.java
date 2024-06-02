4d938ee8509a8e17240333e14509bbfd5d6b2ffd
Molebogengmaine <127528637+Molebogengmaine@users.noreply.github.com>
2024-06-02 07:10:29 +0200

    Initial commit

---
+++ src/main/java/com/example/backend_angular_service/BackendAngularServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_angular_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAngularServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAngularServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_angular_service/BackendAngularServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_angular_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAngularServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
